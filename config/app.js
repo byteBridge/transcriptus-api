3df42bd76392db03c6a0b8d53794d4770b1bd4c5
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-07 14:25:10 +0200

    Add feature to allow api to be accessible from any domain.

--- config/app.js
+++ config/app.js
@@ -4,12 +4,14 @@ module.exports.mount = (app, express) => {
  	// Before anything, congigure the process enmvironment
 	require('./init').setEnvironment()
 
+	const middleware = require('./middleware')
 	const bodyParser = require('body-parser')
 	const logger = require('morgan')
 
 	app.set('port', process.env.PORT)
 
 	/* APP MIDDLEWARE */
+	app.use(middleware.allowDomains)
 	if (process.env.NODE_ENV === 'test') { app.use(logger('dev')) }
 
 	app.use(bodyParser.urlencoded({ extended: false }))
--- config/middleware.js
+++ config/middleware.js
@@ -1,5 +1,18 @@
 'use strict'
 
-module.exports.handle404  = (req, res, next) => {
+function handle404 (req, res, next) {
 	res.json({message: 'Route not found'})
+}
+
+/*
+	Allow the api to be accessed from any domain
+*/
+function allowDomains (req, res, next) {
+	res.header('Access-Cross-Allow-Origin', '*')
+	next()
+}
+
+module.exports = {
+	handle404,
+	allowDomains
 }
