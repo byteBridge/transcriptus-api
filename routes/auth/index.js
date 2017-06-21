fc5c70b0944b2ee8bc0fbb932e86f9a3c27c5b72
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-21 13:52:01 +0200

    Get rid of the routes/auth/index

--- routes/auth/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const login = require('./login')
-const register = require('./register')
-
-router.use('/login', login)
-router.use('/register', register)
-module.exports = router
