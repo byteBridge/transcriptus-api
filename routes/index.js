cfb5a2d79ef869063dbcccf5fc7eeacb07bb52c2
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-21 06:26:56 +0200

    Add a protected route /secret

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 
 'use strict'
-
+const passport = require('../auth/passport')
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
 	res.json({message: 'success'})
+})
+
+router.get('/secret', passport.authenticate('jwt', { session: false }), (req, res) => {
+	res.json('The fact that you saw tis means you are authenticated')
 })
 
 module.exports = router
