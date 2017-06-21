d4959f39f73fa0881cabbf0e8d3082bcb2015131
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-21 06:25:20 +0200

    Add login logic

---
+++ auth/login.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+const userModel = require('../models/users')
+
+module.exports = (req, res) => {
+  userModel.findOne(req.body.username)
+    .then(user => {
+      if(user == null) return res.status(401).json({message: "user not found"})
+      if (userModel.comparePasswords(user.password, req.body.password) === true) {
+        const payload = { username: user.username }
+        const token = jwt.sign(payload, process.env.JWT_SECRET)
+        res.json({messsage: 'success', token})
+      } else {
+        res.status(401).json({message:"passwords did not match"})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message:"Internal server error"})
+    })
+}
