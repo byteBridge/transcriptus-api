ec962ea844c77841de7684fb77e6f101129553ba
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-22 14:37:38 +0200

    MOve the login concern to the user model

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs')
 const knex = require('../database')
+const jwt = require('jsonwebtoken')
 
 module.exports.findOne = (username) => new Promise((resolve, reject) => {
   knex('users').select().where({username})
@@ -14,7 +15,9 @@ module.exports.findOne = (username) => new Promise((resolve, reject) => {
     .catch(err => reject(err))
 })
 
-module.exports.comparePasswords = (reqPass, dbPass) => bcrypt.compareSync(reqPass, dbPass)
+function comparePasswords (reqPass, dbPass) {
+  return bcrypt.compareSync(reqPass, dbPass) 
+}
 
 module.exports.createUser = (user) => new Promise((resolve, reject) => {
   user.password = hashedPassword(user.password)
@@ -27,8 +30,34 @@ function hashedPassword (password) {
   return bcrypt.hashSync(password, 10)
 }
 
+function generateToken (payload) {
+  return jwt.sign(payload, process.env.JWT_SECRET)
+}
+
 module.exports.getAllUsers = () => new Promise((resolve, reject) => {
   knex('users').select()
     .then(resolve)
     .catch(reject)
 })
+
+module.exports.login = (username, password) => new Promise((resolve, reject) => {
+  knex('users').select().where({ username })
+    .then(user => {
+      if (user.length) {
+        if (comparePasswords(password, user[0].password) === true) {
+          return resolve(generateToken({ username: user[0].username }))
+        } else {
+          //invalid password
+          reject({ status: 401 })
+        }
+      }
+      // user not found
+      reject({ status: 401 })
+    })
+
+    .catch(err => {
+      reject()
+    })
+})
+
+module.exports.comparePasswords = comparePasswords
