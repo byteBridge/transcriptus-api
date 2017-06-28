43e8f1ea8e45343bded44f8608d7e8a9d1d6b544
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-28 17:46:56 +0200

    first check for the user's presence before registering

--- api/register/registerController.js
+++ api/register/registerController.js
@@ -7,12 +7,20 @@ module.exports = (req, res) => {
     return res.status(400).json({ message: error.details[0].message})
   }
 
-  userModel.createUser(value)
+  userModel.findOne(value.username)
     .then(user => {
-      res.status(200).json({message: 'successfully created user.', user})
-    })
+      if (user) {
+        res.status(403).json({message: 'user already exists'})
+      } else {
+         userModel.createUser(value)
+          .then(user => {
+            res.status(200).json({message: 'successfully created user.', user})
+          })
 
-    .catch(error => {
-      res.status(500).json({message: 'something happened', error})
+          .catch(error => {
+            res.status(500).json({message: 'something happened', error})
+          })
+      }
     })
+  
 }
