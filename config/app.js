56c4179e2ea317d60e4edca8f1aa80a27ab0ab0e
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-06-30 11:21:42 +0200

    Use helmet.js to send appropriate headers and add some layer of security

--- config/app.js
+++ config/app.js
@@ -8,6 +8,7 @@ module.exports.mount = (app, express) => {
 	const bodyParser = require('body-parser')
 	const logger = require('morgan')
 	const passport = require('../utils/passportService')
+	const helmet = require('helmet')
 
 	app.set('port', process.env.PORT)
 
@@ -15,6 +16,8 @@ module.exports.mount = (app, express) => {
 	app.use(middleware.allowDomains)
 	if (process.env.NODE_ENV === 'development') { app.use(logger('dev')) }
 
+	// some protection via setting appropriate headers
+	app.use(helmet())
 	app.use(passport.initialize())
 	app.use(bodyParser.urlencoded({ extended: false }))
 	app.use(bodyParser.json())
