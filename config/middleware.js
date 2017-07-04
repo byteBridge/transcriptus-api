493c70f5c275bcda78cfe343e02047ff8b6e1045
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2017-07-04 19:10:31 +0200

    Implement the verifyToken function as middleware
     -- For the time being grab the token from the query
         parameters.

--- config/middleware.js
+++ config/middleware.js
@@ -1,4 +1,6 @@
 'use strict'
+const { verifyToken } = require('../utils/authService')
+const { buildResponse } = require('../utils/responseService')
 
 function handle404 (req, res, next) {
 	res.json({message: 'Route not found'})
@@ -12,7 +14,20 @@ function allowDomains (req, res, next) {
 	next()
 }
 
+function authenticate (req, res, next) {
+	// get the token ( from the query for now)
+	const token = req.query.token
+	if (!token) return buildResponse(res, 401, { message: 'No token provided.'})
+
+	verifyToken(token, process.env.JWT_SECRET, (err, decoded) => {
+		if (err) return buildResponse(res, 401, { message: 'Unauthorized' })
+		
+		next()
+	})
+}
+
 module.exports = {
 	handle404,
-	allowDomains
+	allowDomains,
+	authenticate
 }
